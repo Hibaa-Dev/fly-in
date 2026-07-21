4aa4a08abb5a6b3e07b32957a13cddba58dac75d
hiba mezrari <hiba.mezrari@gmail.com>
2026-07-21 02:23:55 +0100

    fly-in

--- fly-in.py
+++ fly-in.py
@@ -6,20 +6,51 @@ from display import Display
 import sys
 
 
+def build_graph(mape_file):
+    graph = Graph()
+    graph.create_zone(mape_file)
+    graph.create_conn(mape_file)
+    return graph
+
+
+def count_turns(mape_file, paths):
+    graph = build_graph(mape_file)
+    simulation = Simulation(mape_file['nb_drones'], graph, paths)
+    simulation.create_drones()
+    simulation.assign_path()
+    simulation.run()
+    return len([line for line in simulation.output if line.strip()])
+
+
+def select_fastest_paths(mape_file, paths):
+    if not paths:
+        return paths
+
+    best_paths = paths
+    best_turns = count_turns(mape_file, paths)
+
+    for path_count in range(1, len(paths)):
+        candidate_paths = paths[:path_count]
+        candidate_turns = count_turns(mape_file, candidate_paths)
+        if candidate_turns < best_turns:
+            best_paths = candidate_paths
+            best_turns = candidate_turns
+
+    return best_paths
+
+
 # try:
 #    --------check mape file--------------
 parser = Parser(sys.argv[1])
 mape_file = parser.check_input_file()
 
 # ----------------Validate--------------------
-graph = Graph()
-graph.create_zone(mape_file)
-graph.create_conn(mape_file)
+graph = build_graph(mape_file)
 
 # -----------------Algo----------------------
 yen = Yen(graph)
 paths = yen.find_shortets_paths()
-print(paths)
+paths = select_fastest_paths(mape_file, paths)
 
 #----------------simulation-------------------
 simulation = Simulation(mape_file['nb_drones'], graph, paths)
